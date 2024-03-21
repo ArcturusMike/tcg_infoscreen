d34396c1203534860aff1c37b4788431428c018a
Hobel Michael <michael.hobel@student.tugraz.at>
2024-03-21 15:42:22 +0100

    vorstandsdienst hardcodiert

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function uhrzeit() {
-    let wochentage = ["So", "Mo", "Di", "Mi", "Do", "Fr", "Sa"];
-    let monate = ["Januar","Februar","März","April","Mai","Juni","Juli","August","September","Oktober","November","Dezember"];
+    let wochentage = ["Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"];
+    let monate = ["01","02","03","04","05","06","07","08","09","10","11","12"];
 
     let today = new Date();
     let wochentag = wochentage[today.getDay()];
     let tag = today.getDate();
     tag = checkTime(tag);
     let monat = monate[today.getMonth()]
-    let date = tag + ". " + monat;
+    let date = tag + "." + monat + ".";
 
     let stunden = today.getHours();
     stunden = checkTime(stunden);
@@ -28,4 +28,46 @@ function checkTime(i) {
         i = "0" + i;  // add zero in front of numbers < 10
     }
     return i;
+}
+
+function vorstandsdienst() {
+    let vorstand = {
+        19: "Rohrmeister Gerald",
+        20: "Mayer Marko",
+        21: "Wernig Rene",
+        22: "Riepl Jan",
+        23: "Nachbar Patrick",
+        24: "Stefan Christopher",
+        25: "Rohrmeister Gerald",
+        26: "Mayer Marko",
+        27: "Wernig Rene",
+        28: "Riepl Jan",
+        29: "Nachbar Patrick",
+        30: "Stefan Christopher",
+        31: "Rohrmeister Gerald",
+        32: "Mayer Marko",
+        33: "Wernig Rene",
+        34: "Riepl Jan",
+        35: "Nachbar Patrick",
+        36: "Stefan Christopher",
+        37: "Mayer Marko",
+        38: "Rohrmeister Gerald",
+        39: "Wernig Rene",
+        40: "Riepl Jan",
+        41: "Nachbar Patrick",
+        42: "Stefan Christopher",
+    };
+
+    const today = new Date();
+    const januaryFirst = new Date(today.getFullYear(), 0, 1);
+    const daysUntilToday = Math.floor((today - januaryFirst) / (24 * 60 * 60 * 1000)) + 1;
+    const weekNumber = Math.ceil((daysUntilToday + januaryFirst.getDay()) / 7);
+
+    let dienstname = vorstand[weekNumber];
+    if (dienstname == undefined) {
+        dienstname = "Riepl Jan";
+    }
+
+
+    document.getElementById("vorstand").innerHTML = dienstname;
 }
