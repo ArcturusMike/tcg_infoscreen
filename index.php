412e43a83d80ba577e8bcc04b466f8457fe6f5e5
Hobel Michael <michael.hobel@student.tugraz.at>
2024-04-13 18:35:44 +0200

    wetter wieder größer und uhrzeit + wochendienst nach unten verschoben

--- index.php
+++ index.php
@@ -17,12 +17,10 @@
                 <div class="container-fluid h-100 bg-danger rounded-3">
                     <div class="row h-100 d-flex align-items-center ps-2 pe-2 text-light">
                         <div class="col p-0 h-100 text-start d-flex align-items-center">
-                            <div class="container-fluid fs-1 fw-semibold" id="uhrzeit"></div>
+                            <!-- <div class="container-fluid fs-1 fw-semibold" id="uhrzeit"></div> -->
                         </div>
                         <div class="col p-0 h-100 text-end d-flex align-items-center">
-                            <div class="container-fluid fs-1 fw-semibold">
-                                Vorstands-Wochendienst:&nbsp;&nbsp;<span class="fs-1 fw-semibold" id="vorstand"></span>
-                            </div>
+                            <!-- <div class="container-fluid fs-1 fw-semibold">Vorstands-Wochendienst:&nbsp;&nbsp;<span class="fs-1 fw-semibold" id="vorstand"></span></div> -->
                         </div>
                     </div>
                 </div>
@@ -52,13 +50,41 @@
                 </div>
             </div>
         </div>
+
+
         <div class="row row-rand" id="row-unten">
-            <div class="col p-2 pb-3 ps-3 pe-3">
-                <div class="container-fluid rounded-3 bg-danger d-flex align-items-center h-100 text-light">
-                    <marquee class="fs-1 fw-semibold"><?php echo file_get_contents("../dateien/lauftext.txt"); ?></marquee>
+            <div class="col p-2 pb-3 ps-3 pe-3">            
+                <div class="container-fluid h-100 bg-danger rounded-3">
+                    <div class="row h-100 d-flex align-items-center ps-2 pe-2 text-light">
+                        <div class="col-3 p-0 h-100 text-start d-flex align-items-center">
+                            <div class="container-fluid fs-1 fw-semibold" id="uhrzeit"></div>
+                        </div>
+                        <div class="col p-0 h-100 text-end d-flex align-items-center">
+                            <div class="container-fluid d-flex align-items-center h-100 fs-1 fw-semibold">|&nbsp;
+                                <marquee>Vorstands-Wochendienst:&nbsp;&nbsp;<span class="fs-1 fw-semibold" id="vorstand"></span> <?php echo file_get_contents("../dateien/lauftext.txt"); ?></marquee>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
+
+
+
+
+
+
+
+        <!--
+        <div class="row row-rand" id="row-unten">
+            <div class="col p-2 pb-3 ps-3 pe-3">
+                <div class="container-fluid rounded-3 bg-danger d-flex align-items-center h-100 fs-1 fw-semibold text-light">
+                    <span id="uhrzeit"></span> | 
+                    <marquee>Vorstands-Wochendienst:&nbsp;&nbsp;<span class="fs-1 fw-semibold" id="vorstand"></span> | <?php echo file_get_contents("../dateien/lauftext.txt"); ?></marquee>
+                </div>
+            </div>
+        </div>
+        -->
     </div>
 </body>
 </html>
