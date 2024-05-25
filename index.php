56bcc6dcaa66d8745fad5bbb99aaecebcdb63ac1
ArcturusMike <meikinator11@gmail.com>
2024-05-25 16:15:56 +0200

    uhrzeit-lauftext nach oben in die mitte

--- index.php
+++ index.php
@@ -35,31 +35,19 @@
             if (file_get_contents("../dateien/praesentationsmodus.txt") == "") {
                 include "pdf-homepage.html";
                 include "platzreservierung.html";
+                include "uhrzeit-lauftext.php";
                 include "meisterschaft-wetter.html";
             }
             // Präsentationsmodus
             else {
                 include "platzreservierung.html";
                 include "praesentation.php";
+                include "uhrzeit-lauftext.php";
             }
+
         ?>
 
-        <div class="row row-rand" id="row-unten">
-            <div class="col-4 p-2 pb-3 ps-3">            
-                <div class="container-fluid h-100 bg-danger rounded-3">
-                    <div class="row h-100 d-flex align-items-center ps-2 pe-2 text-light">
-                        <div class="col p-0 h-100 text-start d-flex align-items-center">
-                            <div class="container-fluid fs-1 fw-semibold" id="uhrzeit"></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col p-2 pb-3 pe-3">            
-                <div class="container-fluid rounded-3 bg-danger d-flex align-items-center h-100 fs-1 fw-semibold text-light">
-                    <marquee scrollamount="10">Vorstands-Wochendienst:&nbsp;&nbsp;<span class="fs-1 fw-semibold" id="vorstand"></span> <?php echo file_get_contents("../dateien/lauftext.txt"); ?></marquee>
-                </div>
-            </div>
-        </div>
+        
 
 
 
